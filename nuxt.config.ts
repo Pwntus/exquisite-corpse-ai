705107ce76409c408655c11eb677ccda75020e8e
Pwntus <pontus.edvard@gmail.com>
2024-08-27 15:26:30 +0300

    feat: increase vercel function timeout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,12 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   ssr: false,
   nitro: {
-    preset: 'vercel'
+    preset: 'vercel',
+    vercel: {
+      functions: {
+        maxDuration: 90
+      }
+    }
   },
   sourcemap: {
     server: false,
